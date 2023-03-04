6ac080d4dcd93d44a1ca60c4a2dc413de74109f6
X <androysol@gmail.com>
2023-03-04 18:14:45 +0300

    feat: Tech section
    Added "tech" section, animated technology balls using three js
    
    refactor: Computers component
    
    Incorrect spelling of "frameLoop" in canvas props

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { SectionWrapper } from '@/hoc/index.js'
+import { technologies } from '@/constants/index.js'
+import { BallCanvas } from '@/components/canvas/index.js'
 
 const Tech = () => {
-	return <div>Tech</div>
+	return (
+		<div className='flex flex-row flex-wrap justify-center gap-10'>
+			{technologies.map(technology => (
+				<div className='w-28 h-28' key={technology.name}>
+					<BallCanvas icon={technology.icon} />
+				</div>
+			))}
+		</div>
+	)
 }
 
-export default Tech
+export default SectionWrapper(Tech, '')
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { Suspense } from 'react'
+import {
+	Decal,
+	Float,
+	OrbitControls,
+	Preload,
+	useTexture
+} from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import CanvasLoader from '@/components/Loader.jsx'
 
-const Ball = () => {
-	return <div>Ball</div>
+const Ball = props => {
+	const [decal] = useTexture([props.imgUrl])
+
+	return (
+		<Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+			<ambientLight intensity={0.25} />
+			<directionalLight position={[0, 0, 0.05]} />
+			<mesh castShadow={true} recieveShadow={true} scale={2.75}>
+				<icosahedronGeometry args={[1, 1]} />
+				<meshStandardMaterial
+					color='#fff8eb'
+					polygonOffset={true}
+					polygonOffsetFactor={-5}
+					flatShading={true}
+				/>
+				<Decal
+					position={[0, 0, 1]}
+					rotation={[2 * Math.PI, 0, 6.25]}
+					flatShading
+					map={decal}
+				/>
+			</mesh>
+		</Float>
+	)
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+	return (
+		<Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls enableZoom={false} />
+				<Ball imgUrl={icon} />
+			</Suspense>
+
+			<Preload all />
+		</Canvas>
+	)
+}
+
+export default BallCanvas
