a8e02cfd645e98e81e1b0b542c09326dacb3348c
X <androysol@gmail.com>
2023-03-04 17:24:00 +0300

    feat: About section
    Added an "about" section, animated skill cards using Tilt and framer
    motion

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,10 @@
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 	const computer = useGLTF('./desktop_pc/scene.gltf')
 
 	return (
@@ -21,8 +21,8 @@ const Computers = () => {
 			/>
 			<primitive
 				object={computer.scene}
-				scale={0.75}
-				position={[0, -3.25, -1.5]}
+				scale={isMobile ? 0.75 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
 				rotation={[-0.01, -0.2, -0.1]}
 			/>
 		</mesh>
@@ -30,6 +30,23 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false)
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia('(max-width: 500px)')
+		setIsMobile(mediaQuery.matches)
+
+		const handleMediaQueryChange = event => {
+			setIsMobile(event.matches)
+		}
+
+		mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+		return () => {
+			mediaQuery.removeEventListener('change', handleMediaQueryChange)
+		}
+	}, [])
+
 	return (
 		<Canvas
 			frameLoop='demand'
@@ -43,7 +60,7 @@ const ComputersCanvas = () => {
 					maxPolarAngle={Math.PI / 2}
 					minPolarAngle={Math.PI / 2}
 				/>
-				<Computers />
+				<Computers isMobile={isMobile} />
 			</Suspense>
 			<Preload all />
 		</Canvas>
